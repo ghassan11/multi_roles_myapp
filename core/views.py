bb92073902f20eb017c4dfa10f86eba7c1b6467f
ghassan11 <alzeabi@gmail.com>
2025-05-31 18:15:12 +0400

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from django.utils.translation import gettext as _
-from .forms import UserRegistrationForm
+from django.contrib.auth.forms import UserCreationForm
+
+def home_view(request):
+    return render(request, "base.html")
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, 'registration/register_done.html')
+            return redirect('login')  # غيّر 'login' إذا كان اسم مسارك مختلف
     else:
-        form = UserRegistrationForm()
+        form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
